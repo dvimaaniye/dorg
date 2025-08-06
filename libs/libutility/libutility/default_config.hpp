235aab7a5dcd75f34ec61b0a79637db69c0afe2c
Gautam Tiwari <gautamtiwari.dev@gmail.com>
2025-08-07 00:35:08 +0530

    updates default config

--- libs/libutility/libutility/default_config.hpp
+++ libs/libutility/libutility/default_config.hpp
@@ -9,15 +9,15 @@ constexpr const char *default_config = R"(# This is the default config.
 #     dir: 3d # (optional, default: name)
 #     override: true # (optional, default: false. When true, it overrides the extensions of the category, extends otherwise)
 #     extensions:
-#       - blender
-#       - fbx
-#       - obj
+#       - .blender
+#       - .fbx
+#       - .obj
 #
 #   - name: jpg
 #     dir: photo/jpg
 #     extensions:
-#       - jpg
-#       - jpeg
+#       - .jpg
+#       - .jpeg
 #
 # NOTE: The override option is only for overriding extensions, everything else (like dir) is overrided automatically. 
 #       Omit a property to not override it.
@@ -32,97 +32,101 @@ constexpr const char *default_config = R"(# This is the default config.
 
 categories:
   - name: audio
-    dir: audio
     extensions:
-      - mp3
-      - wav
-      - flac
-      - aac
-      - ogg
-      - m4a
-      - wma
-      - alac
-      - aiff
-      - opus
+      - .mp3
+      - .wav
+      - .flac
+      - .aac
+      - .ogg
+      - .m4a
+      - .wma
+      - .alac
+      - .aiff
+      - .opus
 
   - name: archive
-    dir: archive 
     extensions:
-      - zip
-      - rar
-      - 7z
-      - tar
-      - iso
-      - cab
-      - arj
+      - .zip
+      - .rar
+      - .7z
+      - .tar
+      - .iso
+      - .cab
+      - .arj
 
   - name: compressed
-    dir: compressed 
     extensions:
-      - gz
-      - bz2
-      - xz
-      - lz
-      - lzma
-      - zst
+      - .gz
+      - .bz2
+      - .xz
+      - .lz
+      - .lzma
+      - .zst
 
   - name: document
-    dir: document 
     extensions:
-      - pdf
-      - doc
-      - docx
-      - txt
-      - odt
-      - rtf
-      - tex
-      - md
+      - .pdf
+      - .doc
+      - .docx
+      - .txt
+      - .odt
+      - .rtf
+      - .tex
+      - .md
 
   - name: photo
-    dir: photo 
     extensions:
-      - jpg
-      - jpeg
-      - png
-      - gif
-      - bmp
-      - tiff
-      - svg
-      - webp
-      - heic
-      - raw
+      - .jpg
+      - .jpeg
+      - .png
+      - .gif
+      - .bmp
+      - .tiff
+      - .svg
+      - .webp
+      - .heic
+      - .raw
 
   - name: presentation
-    dir: presentation 
     extensions:
-      - ppt
-      - pptx
-      - odp
-      - key
+      - .ppt
+      - .pptx
+      - .odp
+      - .key
 
   - name: spreadsheet
-    dir: spreadsheet 
     extensions:
-      - xls
-      - xlsx
-      - xlsm
-      - ods
-      - csv
-      - tsv
-      - xlsb
-      - xlt
-      - xltx
+      - .xls
+      - .xlsx
+      - .xlsm
+      - .ods
+      - .csv
+      - .tsv
+      - .xlsb
+      - .xlt
+      - .xltx
 
   - name: video
-    dir: video 
     extensions:
-      - mp4
-      - mkv
-      - mov
-      - avi
-      - webm
-      - flv
-      - wmv
-      - m4v
-      - 3gp
+      - .mp4
+      - .mkv
+      - .mov
+      - .avi
+      - .webm
+      - .flv
+      - .wmv
+      - .m4v
+      - .3gp
+
+  - name: font
+    extensions:
+      - .ttf
+      - .otf
+      - .woff
+      - .woff2
+      - .eot
+      - .sfnt
+      - .fnt
+      - .pfa
+      - .pfb
 )";
